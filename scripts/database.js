d86704df026e181bcb852c6e8e4d24e7ddf2d3d3
Carolina Pinheiro dos Santos <54641272832@>
2024-11-18 13:12:22 -0300

    18/11/24

---
+++ scripts/database.js
@@ -0,0 +1,145 @@
+export const database = [ /* "Banco de dados" de produtos comercializados no site */
+    {
+        codigoProduto:1,
+        tituloProduto:"Colar de Coração com Pedra Rosa", 
+        preco: 99.90, 
+        descricao:"Este delicado colar traz um pingente em forma de coração, delicadamente adornado com uma pedra rosa que adiciona um toque de suavidade e elegância. Ideal para ocasiões especiais ou para dar um toque de charme ao seu dia a dia, este acessório combina com qualquer estilo, simbolizando amor, carinho e um toque de feminilidade.Perfeito para presentear alguém especial ou para se mimar com um toque de brilho e sofisticação.",
+        imagemProduto: {
+            img1:"./imgens/produtos/colar rosa.png",
+            img2:"./images/products/prod01/img2.png",
+            img3:"./images/products/prod01/img3.png",
+            img4:"./images/products/prod01/img4.png",
+
+        },
+        categoriaProduto: 'Colar',
+        classificacaoProduto:"Novidades", //Perfeitos e Feitos para Você!
+        exibirHome: true
+    },
+    {
+        codigoProduto:2,
+        tituloProduto:"Anel Trançado Cravejado", 
+        preco: 84.90, 
+        descricao:"Este anel delicado apresenta um design trançado único, onde uma parte é sutilmente cravejada com pedras que brilham suavemente, enquanto a outra parte mantém a simplicidade e elegância de um acabamento liso. O contraste entre os dois elementos cria uma harmonia perfeita, trazendo um toque de sofisticação e modernidade ao visual. Ideal para quem busca um acessório discreto, mas cheio de personalidade. Um anel que pode ser usado sozinho ou combinado com outros, para complementar qualquer estilo com muito charme e delicadeza.",
+        imagemProduto: {
+            img1:"./imgens/produtos/anel2.jpg",
+            img2:"./images/products/prod02/img2.png",
+            img3:"./images/products/prod02/img3.png",
+            img4:"./images/products/prod02/img4.png",
+    },
+        categoriaProduto: 'Anel',
+        classificacaoProduto:"Novidades", //Perfeitos e Feitos para Você!
+        exibirHome: true
+    },
+    {
+        codigoProduto:3,
+        tituloProduto:"Relógio Elegante com Cravejados", 
+        preco: 249.90, 
+        descricao:"Este relógio sofisticado combina o design clássico com um toque moderno e delicado. Seus detalhes cravejados em verde água oferecem um brilho suave e uma elegância única, ideal para quem aprecia acessórios refinados. O mostrador é limpo e elegante, com números discretos, enquanto a pulseira, com acabamento de alta qualidade, garante conforto e durabilidade. Um item perfeito para complementar qualquer look, seja no dia a dia ou em ocasiões especiais. Adicione um toque de charme e frescor ao seu visual com este relógio que exala sofisticação.",
+        imagemProduto: {
+            img1:"./imgens/produtos/relogio_pandora.png",
+            img2:"./images/products/prod02/img2.png",
+            img3:"./images/products/prod02/img3.png",
+            img4:"./images/products/prod02/img4.png",
+
+        },
+        categoriaProduto: 'Relógio',
+        classificacaoProduto:"Novidades", //Perfeitos e Feitos para Você!
+        exibirHome: true
+    },
+    {
+        codigoProduto:4,
+        tituloProduto:"Pulseira com Pingente de Coração Rosa", 
+        preco: 89.90, 
+        descricao:"Esta pulseira encantadora traz um pingente de coração, adornado com uma pedra rosa suave, que adiciona um toque de feminilidade e graça. Seu design delicado e atemporal faz dela uma peça perfeita para complementar qualquer look, seja para o dia a dia ou para ocasiões especiais. A pedra rosa simboliza amor e harmonia, tornando esta pulseira uma escolha única para quem busca um acessório cheio de significado e elegância. Ideal para presentear alguém especial ou para se mimar com um toque de sofisticação.",
+        imagemProduto: {
+            img1:"./imgens/produtos/pulseira_coracao.png",
+            img2:"./images/products/prod02/img2.png",
+            img3:"./images/products/prod02/img3.png",
+            img4:"./images/products/prod02/img4.png",
+
+        },
+        categoriaProduto: 'Pulseira',
+        classificacaoProduto:"Novidades", //Perfeitos e Feitos para Você!
+        exibirHome: true
+    },
+    // {
+    //     codigoProduto:5,
+    //     tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
+    //     preco: 300.00, 
+    //     descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+    //     imagemProduto: {
+    //         img1:"./images/products/prod02/img1.png",
+    //         img2:"./images/products/prod02/img2.png",
+    //         img3:"./images/products/prod02/img3.png",
+    //         img4:"./images/products/prod02/img4.png",
+
+    //     },
+    //     categoriaProduto: 'Ferramentas',
+    //     classificacaoProduto:"Mais_Vendidos",
+    //     exibirHome: true
+    // },
+    // {
+    //     codigoProduto:6,
+    //     tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
+    //     preco: 300.00, 
+    //     descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+    //     imagemProduto: {
+    //         img1:"./images/products/prod02/img1.png",
+    //         img2:"./images/products/prod02/img2.png",
+    //         img3:"./images/products/prod02/img3.png",
+    //         img4:"./images/products/prod02/img4.png",
+
+    //     },
+    //     categoriaProduto: 'Ferramentas',
+    //     classificacaoProduto:"Mais_Vendidos",
+    //     exibirHome: true
+    // },
+    // {
+    //     codigoProduto:7,
+    //     tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
+    //     preco: 300.00, 
+    //     descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+    //     imagemProduto: {
+    //         img1:"./images/products/prod02/img1.png",
+    //         img2:"./images/products/prod02/img2.png",
+    //         img3:"./images/products/prod02/img3.png",
+    //         img4:"./images/products/prod02/img4.png",
+
+    //     },
+    //     categoriaProduto: 'Ferramentas',
+    //     classificacaoProduto:"Mais_Vendidos",
+    //     exibirHome: true
+    // },
+    // {
+    //     codigoProduto:8,
+    //     tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
+    //     preco: 300.00, 
+    //     descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+    //     imagemProduto: {
+    //         img1:"./images/products/prod02/img1.png",
+    //         img2:"./images/products/prod02/img2.png",
+    //         img3:"./images/products/prod02/img3.png",
+    //         img4:"./images/products/prod02/img4.png",
+
+    //     },
+    //     categoriaProduto: 'Ferramentas',
+    //     classificacaoProduto:"Mais_Vendidos",
+    //     exibirHome: true
+    // },
+    // {
+    //     codigoProduto:9,
+    //     tituloProduto:"Furadeira e Parafusadeira Bosch Brushless de Impacto a Bateria 12V 3/8 12V GSB 12V-30", 
+    //     preco: 300.00, 
+    //     descricao:"A Parafusadeira e Furadeira de Impacto a Bateria de 3/8 Bosch GSB 12V-30, 12V, possui motor sem escovas de carvão a um preço que você nunca viu! Além disso, vem com sistema inteligente ECP que protege eletronicamente as células da bateria.",
+    //     imagemProduto: {
+    //         img1:"./images/products/prod02/img1.png",
+    //         img2:"./images/products/prod02/img2.png",
+    //         img3:"./images/products/prod02/img3.png",
+    //         img4:"./images/products/prod02/img4.png",
+
+    //     },
+    //     categoriaProduto: 'Ferramentas',
+    //     classificacaoProduto:"Mais_Vendidos",
+    //     exibirHome: true
+    // },
+]
