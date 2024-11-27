a4354ab1ea72df5ab23ea25ee94b1cbea3134508
Carolina Pinheiro dos Santos <54641272832@>
2024-11-27 15:54:54 -0300

    27/11

--- scripts/database.js
+++ scripts/database.js
@@ -1,12 +1,12 @@
 export const database = [ /* "Banco de dados" de produtos comercializados no site */
     {
         codigoProduto:1,
-        tituloProduto:"Colar de Coração com Pedra Rosa", 
+        tituloProduto:"Colar de Mini Coração", 
         preco: 99.90, 
-        descricao:"Este delicado colar traz um pingente em forma de coração, delicadamente adornado com uma pedra rosa que adiciona um toque de suavidade e elegância. Ideal para ocasiões especiais ou para dar um toque de charme ao seu dia a dia, este acessório combina com qualquer estilo, simbolizando amor, carinho e um toque de feminilidade.Perfeito para presentear alguém especial ou para se mimar com um toque de brilho e sofisticação.",
+        descricao:"Este delicado colar traz em forma de coração. Ideal para ocasiões especiais ou para dar um toque de charme ao seu dia a dia, este acessório combina com qualquer estilo, simbolizando amor, carinho e um toque de feminilidade.Perfeito para presentear alguém especial ou para se mimar com um toque de brilho e sofisticação.",
         imagemProduto: {
-            img1:"./imgens/produtos/colar rosa.png",
-            img2:"./images/products/prod01/img2.png",
+            img1:"./imgens/produtos/colar_mini_coracao_1.png",
+            img2:"./imgens/produtos/colar_mini_coracao_2.png",
             img3:"./images/products/prod01/img3.png",
             img4:"./images/products/prod01/img4.png",
 
@@ -17,12 +17,12 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:2,
-        tituloProduto:"Anel Trançado Cravejado", 
+        tituloProduto:"Anel Cravejado", 
         preco: 84.90, 
         descricao:"Este anel delicado apresenta um design trançado único, onde uma parte é sutilmente cravejada com pedras que brilham suavemente, enquanto a outra parte mantém a simplicidade e elegância de um acabamento liso. O contraste entre os dois elementos cria uma harmonia perfeita, trazendo um toque de sofisticação e modernidade ao visual. Ideal para quem busca um acessório discreto, mas cheio de personalidade. Um anel que pode ser usado sozinho ou combinado com outros, para complementar qualquer estilo com muito charme e delicadeza.",
         imagemProduto: {
-            img1:"./imgens/produtos/anel2.jpg",
-            img2:"./images/products/prod02/img2.png",
+            img1:"./imgens/produtos/anel.png",
+            img2:"./imgens/produtos/anel2.png",
             img3:"./images/products/prod02/img3.png",
             img4:"./images/products/prod02/img4.png",
     },
@@ -32,28 +32,28 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:3,
-        tituloProduto:"Relógio Elegante com Cravejados", 
-        preco: 249.90, 
-        descricao:"Este relógio sofisticado combina o design clássico com um toque moderno e delicado. Seus detalhes cravejados em verde água oferecem um brilho suave e uma elegância única, ideal para quem aprecia acessórios refinados. O mostrador é limpo e elegante, com números discretos, enquanto a pulseira, com acabamento de alta qualidade, garante conforto e durabilidade. Um item perfeito para complementar qualquer look, seja no dia a dia ou em ocasiões especiais. Adicione um toque de charme e frescor ao seu visual com este relógio que exala sofisticação.",
+        tituloProduto:"Brinco Princess", 
+        preco: 34.90, 
+        descricao: "Este brinco delicado de princesa é a perfeita combinação de elegância e sofisticação. Com um design refinado, ele apresenta uma pequena pedra central de zircona cúbica, que captura a luz com seu brilho sutil e radiante. Suas linhas suaves e detalhes delicados transmitem um toque de nobreza, tornando-o ideal para ocasiões especiais ou para adicionar um toque de charme ao dia a dia. Perfeito para quem busca um acessório clássico, atemporal e cheio de graça.",
         imagemProduto: {
-            img1:"./imgens/produtos/relogio_pandora.png",
-            img2:"./images/products/prod02/img2.png",
+            img1:"./imgens/produtos/brinco_coracao.png",
+            img2:"./imgens/produtos/brinco_coracao2.png",
             img3:"./images/products/prod02/img3.png",
             img4:"./images/products/prod02/img4.png",
 
         },
-        categoriaProduto: 'Relógio',
+        categoriaProduto: 'Brinco',
         classificacaoProduto:"Novidades", //Perfeitos e Feitos para Você!
         exibirHome: true
     },
     {
         codigoProduto:4,
-        tituloProduto:"Pulseira com Pingente de Coração Rosa", 
+        tituloProduto:"Pulseira com Pingente de Coração", 
         preco: 89.90, 
-        descricao:"Esta pulseira encantadora traz um pingente de coração, adornado com uma pedra rosa suave, que adiciona um toque de feminilidade e graça. Seu design delicado e atemporal faz dela uma peça perfeita para complementar qualquer look, seja para o dia a dia ou para ocasiões especiais. A pedra rosa simboliza amor e harmonia, tornando esta pulseira uma escolha única para quem busca um acessório cheio de significado e elegância. Ideal para presentear alguém especial ou para se mimar com um toque de sofisticação.",
+        descricao:"Esta pulseira encantadora traz um pingente de coração, que adiciona um toque de feminilidade e graça. Seu design delicado e atemporal faz dela uma peça perfeita para complementar qualquer look, seja para o dia a dia ou para ocasiões especiais. A pedra rosa simboliza amor e harmonia, tornando esta pulseira uma escolha única para quem busca um acessório cheio de significado e elegância. Ideal para presentear alguém especial ou para se mimar com um toque de sofisticação.",
         imagemProduto: {
             img1:"./imgens/produtos/pulseira_coracao.png",
-            img2:"./images/products/prod02/img2.png",
+            img2:"./imgens/produtos/pulseira_coracao2.png",
             img3:"./images/products/prod02/img3.png",
             img4:"./images/products/prod02/img4.png",
 
@@ -62,69 +62,7 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         classificacaoProduto:"Novidades", //Perfeitos e Feitos para Você!
         exibirHome: true
     },
-    {
-        codigoProduto:1,
-        tituloProduto:"Colar de Coração com Pedra Rosa", 
-        preco: 99.90, 
-        descricao:"Este delicado colar traz um pingente em forma de coração, delicadamente adornado com uma pedra rosa que adiciona um toque de suavidade e elegância. Ideal para ocasiões especiais ou para dar um toque de charme ao seu dia a dia, este acessório combina com qualquer estilo, simbolizando amor, carinho e um toque de feminilidade.Perfeito para presentear alguém especial ou para se mimar com um toque de brilho e sofisticação.",
-        imagemProduto: {
-            img1:"./imgens/produtos/colar rosa.png",
-            img2:"./images/products/prod01/img2.png",
-            img3:"./images/products/prod01/img3.png",
-            img4:"./images/products/prod01/img4.png",
-
-        },
-        categoriaProduto: 'Colar',
-        classificacaoProduto:"Novidades", //Perfeitos e Feitos para Você!
-        exibirHome: true
-    },
-    {
-        codigoProduto:2,
-        tituloProduto:"Anel Trançado Cravejado", 
-        preco: 84.90, 
-        descricao:"Este anel delicado apresenta um design trançado único, onde uma parte é sutilmente cravejada com pedras que brilham suavemente, enquanto a outra parte mantém a simplicidade e elegância de um acabamento liso. O contraste entre os dois elementos cria uma harmonia perfeita, trazendo um toque de sofisticação e modernidade ao visual. Ideal para quem busca um acessório discreto, mas cheio de personalidade. Um anel que pode ser usado sozinho ou combinado com outros, para complementar qualquer estilo com muito charme e delicadeza.",
-        imagemProduto: {
-            img1:"./imgens/produtos/anel2.jpg",
-            img2:"./images/products/prod02/img2.png",
-            img3:"./images/products/prod02/img3.png",
-            img4:"./images/products/prod02/img4.png",
-    },
-        categoriaProduto: 'Anel',
-        classificacaoProduto:"Novidades", //Perfeitos e Feitos para Você!
-        exibirHome: true
-    },
-    {
-        codigoProduto:3,
-        tituloProduto:"Relógio Elegante com Cravejados", 
-        preco: 249.90, 
-        descricao:"Este relógio sofisticado combina o design clássico com um toque moderno e delicado. Seus detalhes cravejados em verde água oferecem um brilho suave e uma elegância única, ideal para quem aprecia acessórios refinados. O mostrador é limpo e elegante, com números discretos, enquanto a pulseira, com acabamento de alta qualidade, garante conforto e durabilidade. Um item perfeito para complementar qualquer look, seja no dia a dia ou em ocasiões especiais. Adicione um toque de charme e frescor ao seu visual com este relógio que exala sofisticação.",
-        imagemProduto: {
-            img1:"./imgens/produtos/relogio_pandora.png",
-            img2:"./images/products/prod02/img2.png",
-            img3:"./images/products/prod02/img3.png",
-            img4:"./images/products/prod02/img4.png",
-
-        },
-        categoriaProduto: 'Relógio',
-        classificacaoProduto:"Novidades", //Perfeitos e Feitos para Você!
-        exibirHome: true
-    },
-    {
-        codigoProduto:4,
-        tituloProduto:"Pulseira com Pingente de Coração Rosa", 
-        preco: 89.90, 
-        descricao:"Esta pulseira encantadora traz um pingente de coração, adornado com uma pedra rosa suave, que adiciona um toque de feminilidade e graça. Seu design delicado e atemporal faz dela uma peça perfeita para complementar qualquer look, seja para o dia a dia ou para ocasiões especiais. A pedra rosa simboliza amor e harmonia, tornando esta pulseira uma escolha única para quem busca um acessório cheio de significado e elegância. Ideal para presentear alguém especial ou para se mimar com um toque de sofisticação.",
-        imagemProduto: {
-            img1:"./imgens/produtos/pulseira_coracao.png",
-            img2:"./images/products/prod02/img2.png",
-            img3:"./images/products/prod02/img3.png",
-            img4:"./images/products/prod02/img4.png",
-
-        },
-        categoriaProduto: 'Pulseira',
-        classificacaoProduto:"Novidades", //Perfeitos e Feitos para Você!
-        exibirHome: true
-    },
+    
     {
         codigoProduto:5,
         tituloProduto:"Colar Prata com Pingente Olho Grego",
@@ -132,7 +70,7 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         descricao:"O colar prata com pingente olho grego é uma peça elegante e cheia de significado, que combina estilo e proteção. Confeccionado em prata de alta qualidade, o colar apresenta um design delicado e sofisticado, perfeito para quem busca um acessório versátil e cheio de simbolismo. O pingente em formato de olho grego, conhecido por sua associação com a proteção contra energias negativas, adiciona um toque de misticismo à peça. Ideal para o uso diário ou para ocasiões especiais, este colar é uma excelente escolha para quem valoriza tanto a beleza quanto a tradição.",
         imagemProduto: {
             img1:"./imgens/produtos/colar_olho.png",
-            img2:"./images/products/prod02/img2.png",
+            img2:"./imgens/produtos/colar_olho2.png",
             img3:"./images/products/prod02/img3.png",
             img4:"./images/products/prod02/img4.png",
 
@@ -148,7 +86,7 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         descricao:"O brinco argola pequena é uma peça clássica e atemporal, que adiciona um toque de sofisticação discreta ao seu visual. Com um design delicado e elegante, esse brinco é perfeito para o uso diário, combinando com qualquer estilo ou ocasião. Sua argola pequena oferece conforto e versatilidade, sendo ideal para quem busca um acessório sutil, mas impactante. Feito com materiais de alta qualidade, o brinco argola pequena é a escolha perfeita para quem deseja um toque de charme e modernidade, sem perder a leveza e a sofisticação.",
         imagemProduto: {
             img1:"./imgens/produtos/brinco_torçao.png",
-            img2:"./images/products/prod02/img2.png",
+            img2:"./imgens/produtos/brinco_torçao2.png",
             img3:"./images/products/prod02/img3.png",
             img4:"./images/products/prod02/img4.png",
 
@@ -164,7 +102,7 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
         descricao:"A pulseira com cristais é uma peça delicada e sofisticada, que traz um toque de elegância e brilho ao seu visual. Confeccionada com cristais de alta qualidade, ela reflete a luz de maneira sutil, criando um efeito radiante que atrai olhares. Perfeita para adicionar charme e glamour ao dia a dia ou para complementar um look de festa, esta pulseira combina com diversos estilos e ocasiões. Seu design versátil e atemporal faz dela uma escolha ideal para quem busca um acessório que une beleza e sofisticação, com um toque de luxo discreto.",
         imagemProduto: {
             img1:"./imgens/produtos/Pulseira_cristal.png",
-            img2:"./images/products/prod02/img2.png",
+            img2:"./imgens/produtos/Pulseira_cristal2.png",
             img3:"./images/products/prod02/img3.png",
             img4:"./images/products/prod02/img4.png",
 
@@ -175,12 +113,12 @@ export const database = [ /* "Banco de dados" de produtos comercializados no sit
     },
     {
         codigoProduto:8,
-        tituloProduto:"Brinco com Mini Corações", 
-        preco: 69.90, 
-        descricao:"",
+        tituloProduto:"Brinco Organic", 
+        preco: 39.90, 
+        descricao:"Este brinco orgânico é uma verdadeira expressão de estilo e sustentabilidade, criado para quem busca beleza com um toque natural e único. Feito a partir de materiais cuidadosamente selecionados, como madeira, sementes, fibras naturais ou metais reciclados, ele combina elegância e respeito ao meio ambiente. Seu design, inspirado nas formas e texturas da natureza, transmite um ar de autenticidade e conexão com o planeta. Ideal para quem valoriza acessórios que refletem a harmonia entre arte, natureza e consciência ecológica. Versátil e leve, o brinco orgânico é perfeito para qualquer ocasião, trazendo um charme discreto e sofisticado.",
         imagemProduto: {
-            img1:"./imgens/produtos/brinco_coracao.png",
-            img2:"./images/products/prod02/img2.png",
+            img1:"./imgens/produtos/brinco_leque.png",
+            img2:"./imgens/produtos/brinco_leque2.png",
             img3:"./images/products/prod02/img3.png",
             img4:"./images/products/prod02/img4.png",
 
